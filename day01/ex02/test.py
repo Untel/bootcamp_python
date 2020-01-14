3c9943d2fd87d45a8425ed05a4fb46b572dbfb74
Adrien DA SILVA FERNANDES <adda-sil@student.42.fr>
2020-01-14 16:25:47 +0100

    End ex02

---
+++ day01/ex02/test.py
@@ -0,0 +1,18 @@
+from vector import Vector
+# try:
+v1 = Vector([1.0, 1.0, 1.0])
+v2 = Vector([1.0, 2.0, 1.0])
+v3 = Vector((10, 14))
+v4 = Vector(9)
+    # v9 = v1 * v2
+# except Exception:
+#     print("Error")
+print("v1", v1)
+print('v2', v2)
+print('v3', v3)
+print('v4', v4)
+print('v1 * 2', v1 * 2)
+print('v1 + 1', v1 + 1)
+print('v2 - 1', v2 - 1)
+print('v1 / 2', v1 / 2)
+print('1 - v1', 1 - v1)
---
+++ day01/ex02/vector.py
@@ -0,0 +1,59 @@
+import sys
+import string
+
+class Vector:
+    def __init__(self, values):
+        if type(values) is list:
+            self.values = values
+        elif type(values) is int:
+            self.values = [float(value) for value in range(values)]
+        elif type(values) is tuple:
+            self.values = [float(value) for value in range(values[0], values[1])]
+        else:
+            raise Exception("Wrong value type")
+        self.size = len(self.values)
+
+    def __mul__(self, per):
+        if type(per) is int or type(per) is float:
+            return Vector([value * per for value in self.values])
+        else:
+            raise Exception("Cannot multiply a Vector by something else than int or float")
+
+    def __rmul__(self, per):
+        return self.__mul__(per)
+
+    def __add__(self, per):
+        if type(per) is int or type(per) is float:
+            return Vector([value + per for value in self.values])
+        else:
+            raise Exception("Cannot add a Vector by something else than int or float")
+
+    def __radd__(self, per):
+        return self.__add__(per)        
+
+    def __sub__(self, per):
+        if type(per) is int or type(per) is float:
+            return Vector([value - per for value in self.values])
+        else:
+            raise Exception("Cannot sub a Vector by something else than int or float")
+
+    def __rsub__(self, per):
+        return self.__sub__(per)
+
+    def __truediv__(self, per):
+        if type(per) is int or type(per) is float:
+            return Vector([value / per for value in self.values])
+        else:
+            raise Exception("Cannot multiply a Vector by something else than int or float")
+
+    def __rtruediv__(self, per):
+        return self.__truediv__(per)
+
+    def __str__(self):
+        return "Size: " + str(self.size) + " // " + str(self.values)
+
+    def __repr__(self):
+        return {
+            'size': self.size,
+            'values': self.values
+        }
